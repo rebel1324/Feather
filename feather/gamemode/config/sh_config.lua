730b4d26f1d8fb4cd68f25a1758ae9543463eacc
Chessnut <chessnutist@gmail.com>
2014-06-14 16:11:54 -0700

    Added config file.

---
+++ feather/gamemode/config/sh_config.lua
@@ -0,0 +1,29 @@
+--[[
+	WARNING: DO NOT EDIT ANY OF THESE VALUES! THESE ARE FOR
+	SETTING UP THE CONFIG ONLY, NOT ACTUALLY MODIFYING THE VALUES!
+--]]
+
+feather.config.create("language", "english")
+feather.config.create("chatRange", 512)
+feather.config.create("salary", 50)
+feather.config.create("chatColor", Color(64, 142, 200))
+feather.config.create("jobChangeDelay", 5)
+feather.config.create("arrestTime", 300)
+feather.config.create("wantedTime", 300)
+feather.config.create("printerTime", {1.6, 3})
+feather.config.create("printerExplodeRate", 10)
+feather.config.create("moneyModel", "models/props/cs_assault/money.mdl")
+feather.config.create("doorPrice", 50)
+feather.config.create("lockPickTime", 5)
+feather.config.create("hunger", true)
+feather.config.create("hungerTime", 300)
+feather.config.create("hungerRate", 2)
+feather.config.create("currency", "$")
+feather.config.create("useMail", false)
+feather.config.create("defaultLaws", {
+	"You must obey the admins.",
+	"You must not kill."
+})
+feather.config.create("hitCost", 500)
+feather.config.create("moneyTime", 300)
+feather.config.create("maxDoors", 10)
